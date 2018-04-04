4aa9d2e265737fbd9b28c463912b5eb063c6fcad
Etavern <elijahT1023@gmail.com>
2018-04-04 15:51:47 -0400

    scripts

--- payloads.py
+++ payloads.py
@@ -3,14 +3,13 @@ import sys
 
 target = '127.0.0.1'
 port = 6660
-msg = str(sys.argv[1])
-num = int(sys.argv[2])
+msg = open(sys.argv[1])
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target, port))
 
-client.send(msg * num)
+client.send(msg.read())
 response = client.recv(4096)
 
 print(response)
